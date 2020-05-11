1c594c84ffccd7519449be9d7482a14a8ebc609b
DeepWolf413 <jesper15fuji@live.dk>
2020-05-12 00:45:15 +0200

    Implemented a component that auto disables the audio source when it is done playing the audio clip.

---
+++ Assets/Scripts/AutoDisableAudioSource.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace DeepWolf.SpaceSurvivor
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AutoDisableAudioSource : MonoBehaviour
+    {
+        private AudioSource cachedAudioSource = null;
+
+        private bool isDisabling = false;
+        
+        private void Awake() => cachedAudioSource = GetComponent<AudioSource>();
+
+        private void Update()
+        {
+            if (!isDisabling && cachedAudioSource.clip != null)
+            {
+                Invoke(nameof(Disable), cachedAudioSource.clip.length);
+                isDisabling = true;
+            }
+        }
+
+        private void Disable()
+        {
+            gameObject.SetActive(false);
+            isDisabling = false;
+        }
+    }
+}
