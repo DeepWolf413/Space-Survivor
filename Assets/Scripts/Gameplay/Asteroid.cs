2eb4371dfb8a7edc57eae562cf1146a44d8ef614
DeepWolf413 <jesper15fuji@live.dk>
2020-05-20 23:25:39 +0200

    Implemented asteroid destruction (spawns small asteroids when destroyed), and tweaked the asteroids' data. Also made a small explosion particle for asteroids.

--- Assets/Scripts/Gameplay/Asteroid.cs
+++ Assets/Scripts/Gameplay/Asteroid.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using DeepWolf.SpaceSurvivor.Managers;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace DeepWolf.SpaceSurvivor.Gameplay
 {
@@ -10,6 +13,25 @@ namespace DeepWolf.SpaceSurvivor.Gameplay
 
         [SerializeField]
         private float angularVelocity = 100.0f;
+
+        [SerializeField]
+        private bool setInitialVelocity = true;
+        
+        [Header("[Destruction]")]
+        [SerializeField]
+        private bool spawnPiecesOnDestroy = false;
+
+        [SerializeField]
+        private float piecesMaxSpawnDistance = 1.0f;
+
+        [SerializeField, Tooltip("The amount of pieces to spawn when this asteroid is destroyed. NOTE: This is only used if 'Spawn Pieces On Destroy' is enabled.")]
+        private int piecesSpawnAmount;
+
+        [SerializeField, Tooltip("An array of possible pieces to spawn when this asteroid is destroyed. NOTE: This is only used if 'Spawn Pieces On Destroy' is enabled.")]
+        private Rigidbody2D[] piecesToSpawn = new Rigidbody2D[0];
+
+        [SerializeField]
+        private float explosionForce = 1.5f;
 
         private Rigidbody2D cachedRigidbody = null;
 
@@ -23,11 +45,38 @@ namespace DeepWolf.SpaceSurvivor.Gameplay
             GameObject playerObject = GameObject.FindWithTag("Player");
             if (!playerObject)
             { return; }
-            
-            float travelSpeed = Random.Range(travelSpeedRange.x, travelSpeedRange.y);
-            
-            cachedRigidbody.velocity = (playerObject.transform.position - transform.position).normalized * travelSpeed;
-            cachedRigidbody.angularVelocity = angularVelocity;
+
+            if (setInitialVelocity)
+            {
+                float travelSpeed = Random.Range(travelSpeedRange.x, travelSpeedRange.y);
+                cachedRigidbody.velocity = (playerObject.transform.position - transform.position).normalized * travelSpeed;
+                cachedRigidbody.angularVelocity = angularVelocity;
+            }
         }
+
+        private void OnDestroy()
+        {
+            if (!spawnPiecesOnDestroy)
+            { return; }
+
+            if (!GameManager.Instance || GameManager.Instance.IsApplicationQuitting)
+            { return; }
+            
+            SpawnPieces();
+        }
+
+        private void SpawnPieces()
+        {
+            for (int i = 0; i < piecesSpawnAmount; i++)
+            {
+                Vector3 spawnPos = transform.position + (Vector3)Random.insideUnitCircle * piecesMaxSpawnDistance;
+                Rigidbody2D asteroidPiece = Instantiate(GetRndPiece(), spawnPos, Quaternion.identity);
+
+                Vector2 direction = asteroidPiece.transform.position - transform.position;
+                asteroidPiece.AddForceAtPosition(direction.normalized * explosionForce, transform.position, ForceMode2D.Impulse);
+            }
+        }
+
+        private Rigidbody2D GetRndPiece() => piecesToSpawn[Random.Range(0, piecesToSpawn.Length)];
     }
 }
