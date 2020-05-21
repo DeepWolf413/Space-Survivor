4777a4e396815626131dfebe9f69696c4030a544
DeepWolf413 <jesper15fuji@live.dk>
2020-05-21 23:27:06 +0200

    Implemented a label that shows the space credits.

---
+++ Assets/Scripts/UI/Menues/Ship Selection/SpaceCreditsLabel.cs
@@ -0,0 +1,35 @@
+﻿using DeepWolf.SpaceSurvivor.Managers;
+using TMPro;
+using UnityEngine;
+
+namespace DeepWolf.SpaceSurvivor.UI
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class SpaceCreditsLabel : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI label = null;
+
+        private void OnValidate()
+        {
+            if (!label)
+            { label = GetComponent<TextMeshProUGUI>(); }
+        }
+
+        private void OnEnable()
+        {
+            GameManager.SaveManager.SaveState.SpaceCreditsChanged += OnSpaceCreditsChanged;
+            RefreshLabel();
+        }
+
+        private void OnDisable()
+        {
+            if (!GameManager.IsApplicationQuitting)
+            { GameManager.SaveManager.SaveState.SpaceCreditsChanged -= OnSpaceCreditsChanged; }
+        }
+
+        private void RefreshLabel() => label.text = GameManager.SaveManager.SaveState.SpaceCredits.ToString();
+
+        private void OnSpaceCreditsChanged(int newValue, int oldValue) => RefreshLabel();
+    }
+}
