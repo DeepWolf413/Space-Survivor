c2e11a91882ef1a908c37dd10eaf2dc0c7146606
DeepWolf413 <jesper15fuji@live.dk>
2020-05-21 23:29:19 +0200

    Now asteroids and enemy ships gives Space Credits when destroyed.

---
+++ Assets/Scripts/Gameplay/GiveSpaceCreditsOnDestroy.cs
@@ -0,0 +1,20 @@
+﻿using DeepWolf.SpaceSurvivor.Managers;
+using UnityEngine;
+
+namespace DeepWolf.SpaceSurvivor.Gameplay
+{
+    public class GiveSpaceCreditsOnDestroy : MonoBehaviour
+    {
+        [SerializeField]
+        private int amountToGive = 25;
+
+        private void OnDestroy()
+        {
+            if (GameManager.IsApplicationQuitting)
+            { return; }
+
+            if (ReferenceManager.TryGet(out GameSession gameSession))
+            { gameSession.AddSpaceCreditsReward(amountToGive); }
+        }
+    }
+}
