6be59752248153ced6366d052c951bffa513b117
DeepWolf413 <jesper15fuji@live.dk>
2020-05-25 00:37:24 +0200

    Added some documentation.

--- Assets/Scripts/Gameplay/Pooling/PoolManager.cs
+++ Assets/Scripts/Gameplay/Pooling/PoolManager.cs
@@ -17,12 +17,31 @@ namespace DeepWolf.SpaceSurvivor.Gameplay
 
         #endregion
 
+        /// <summary>
+        /// Spawns a gameobject from the specified pool.
+        /// </summary>
+        /// <param name="poolData">The pool to spawn from.</param>
+        /// <param name="position">The position to place the gameobject at.</param>
+        /// <param name="rotation">The rotation to place the gameobject on.</param>
+        /// <returns>The gameobject that was spawned from the pool.</returns>
         public static GameObject Spawn(PoolData poolData, Vector3 position, Quaternion rotation) =>
             Instance.GetPool(poolData).Spawn(position, rotation);
         
+        /// <summary>
+        /// Spawns a gameobject from the specified pool. This will use GetComponent() to get the wanted component.
+        /// </summary>
+        /// <param name="poolData">The pool to spawn from.</param>
+        /// <param name="position">The position to place the gameobject at.</param>
+        /// <param name="rotation">The rotation to place the gameobject on.</param>
+        /// <typeparam name="T">Which component to return.</typeparam>
+        /// <returns>The component of type <typeparamref name="T"/> from the gameobject that was spawned from the pool.</returns>
         public static T Spawn<T>(PoolData poolData, Vector3 position, Quaternion rotation) where T : Object =>
             Instance.GetPool(poolData).Spawn(position, rotation).GetComponent<T>();
 
+        /// <summary>
+        /// Despawns the <paramref name="objectToDespawn"/>.
+        /// </summary>
+        /// <param name="objectToDespawn"></param>
         public static void Despawn(GameObject objectToDespawn) => objectToDespawn.SetActive(false);
 
         /// <summary>
@@ -38,6 +57,11 @@ namespace DeepWolf.SpaceSurvivor.Gameplay
             pool.transform.SetParent(Instance.transform);
         }
 
+        /// <summary>
+        /// Creates a gameobject and adds a component of type <see cref="Pool"/> to it. Then adds it to the <see cref="pools"/> list.
+        /// </summary>
+        /// <param name="poolData">The pool to create.</param>
+        /// <returns>The <see cref="Pool"/> gameobject that was created.</returns>
         private static Pool CreatePool(PoolData poolData)
         {
             GameObject poolObject = new GameObject($"Pool - {poolData.name}");
