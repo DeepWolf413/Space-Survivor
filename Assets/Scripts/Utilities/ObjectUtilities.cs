7a8b0ba5ff6c32cf3bf7ab1c6d3f5c5c849efc8b
DeepWolf413 <jesper15fuji@live.dk>
2020-05-25 16:39:32 +0200

    Moved the script to the right folder. Also added some error messages in case no objects were found.

--- Assets/Scripts/Utilities/ObjectUtilities.cs
+++ Assets/Scripts/Utilities/ObjectUtilities.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using Logger = DeepWolf.Logging.Logger;
 
 namespace DeepWolf.SpaceSurvivor.Utilities
 {
@@ -11,24 +12,36 @@ namespace DeepWolf.SpaceSurvivor.Utilities
 		public static bool TryGetObjectOfType<T>(out T objectFound) where T : Object
 		{
 			objectFound = Object.FindObjectOfType<T>();
+			if (objectFound == null)
+			{ Logger.LogError($"Couldn't find an object of type '{typeof(T).Name}'."); }
+			
 			return objectFound != null;
 		}
 		
 		public static bool TryGetObjectsOfType<T>(out T[] objectsFound) where T : Object
 		{
 			objectsFound = Object.FindObjectsOfType<T>();
-			return objectsFound != null;
+			if (objectsFound == null || objectsFound.Length == 0)
+			{ Logger.LogError($"Couldn't find any objects of type '{typeof(T).Name}'."); }
+			
+			return objectsFound != null && objectsFound.Length > 0;
 		}
 		
 		public static bool TryGetWithTag(string tag, out GameObject objectFound)
 		{
 			objectFound = GameObject.FindWithTag(tag);
+			if (objectFound == null)
+			{ Logger.LogError($"Couldn't find an object with tag '{tag}'."); }
+			
 			return objectFound != null;
 		}
 		
 		public static bool TryGetObjectsWithTag(string tag, out GameObject[] objectsFound)
 		{
 			objectsFound = GameObject.FindGameObjectsWithTag(tag);
+			if (objectsFound == null)
+			{ Logger.LogError($"Couldn't find any objects with tag '{tag}'."); }
+			
 			return objectsFound != null;
 		}
 	}
