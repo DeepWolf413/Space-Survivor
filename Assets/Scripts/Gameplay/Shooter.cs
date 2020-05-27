55a431b1ede824ecf33161cd64f381a62f9f4ed1
DeepWolf413 <jesper15fuji@live.dk>
2020-05-27 21:20:41 +0200

    Implemented the weapon change behaviour on the Shooter script. I actually forgot to commit this earlier with the pickup system commit, they are related.

--- Assets/Scripts/Gameplay/Shooter.cs
+++ Assets/Scripts/Gameplay/Shooter.cs
@@ -1,15 +1,50 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Logger = DeepWolf.Logging.Logger;
 
 namespace DeepWolf.SpaceSurvivor.Gameplay
 {
     public class Shooter : MonoBehaviour
     {
         [SerializeField]
+        private WeaponData startWeapon = null;
+
+        [SerializeField]
+        private WeaponBehaviour[] weapons = new WeaponBehaviour[0];
+
         private WeaponBehaviour currentWeapon = null;
 
         public WeaponData WeaponData => currentWeapon.Data;
-        
-        public void ChangeWeapon(WeaponBehaviour newWeapon) => currentWeapon = newWeapon;
+
+        private void OnValidate()
+        {
+            if (weapons.Length == 0)
+            { weapons = GetComponentsInChildren<WeaponBehaviour>(); }
+        }
+
+        private void Start()
+        {
+            if (startWeapon == null)
+            { Logger.LogError("No start weapon was specified. Please assign a start weapon."); }
+            else
+            { ChangeWeapon(startWeapon); }
+        }
+
+        public void ChangeWeapon(WeaponData newWeapon)
+        {
+            for (int i = 0; i < weapons.Length; i++)
+            {
+                if (weapons[i].Data != newWeapon)
+                { continue; }
+
+                if (currentWeapon != null)
+                { currentWeapon.gameObject.SetActive(false); }
+                
+                currentWeapon = weapons[i];
+                currentWeapon.gameObject.SetActive(true);
+                return;
+            }
+        }
 
         public void BeginShoot()
         {
