12a2cd9c2e5f84d68be965990beca1f21317513a
DeepWolf413 <jesper15fuji@live.dk>
2020-05-28 17:50:18 +0200

    Implemented UI for active powerups.

---
+++ Assets/Scripts/UI/Gameplay/Powerup/ActivePowerupUI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using DeepWolf.SpaceSurvivor.Gameplay.PowerupSystem;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DeepWolf.SpaceSurvivor.UI
+{
+    public class ActivePowerupUI : MonoBehaviour
+    {
+        [SerializeField]
+        private Slider timer = null;
+
+        [SerializeField]
+        private Image icon = null;
+
+        private ActivePowerup representedPowerup;
+
+        public void Show(ActivePowerup powerup)
+        {
+            gameObject.SetActive(true);
+            representedPowerup = powerup;
+            icon.sprite = powerup.Powerup.Sprite;
+
+            InvokeRepeating(nameof(RefreshTimer), 0.1f, 0.3f);
+            Invoke(nameof(Hide), powerup.TimeLeft);
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+            CancelInvoke(nameof(RefreshTimer));
+        }
+
+        public bool IsRepresentingPowerup(ActivePowerup powerup) => representedPowerup.Powerup == powerup.Powerup;
+
+        private void RefreshTimer()
+        {
+            float powerupDuration = representedPowerup.Powerup.Duration;
+            timer.value = Mathf.Lerp(1.0f, 0.0f, representedPowerup.TimeLeft / powerupDuration);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Gameplay/Powerup/ActivePowerupsListUI.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using DeepWolf.SpaceSurvivor.Gameplay.PowerupSystem;
+using DeepWolf.SpaceSurvivor.Utilities;
+using UnityEngine;
+
+namespace DeepWolf.SpaceSurvivor.UI
+{
+    public class ActivePowerupsListUI : MonoBehaviour
+    {
+        [SerializeField]
+        private ActivePowerupUI activePowerupUIPrefab = null;
+
+        [SerializeField]
+        private RectTransform activePowerupContainer = null;
+
+        private List<ActivePowerupUI> pooledUI = new List<ActivePowerupUI>();
+
+        private void OnEnable()
+        {
+            if (!ObjectUtilities.TryGetWithTag("Player", out GameObject playerObject))
+            { return; }
+            
+            if (!playerObject.TryGetComponent(out PowerupsController powerupsController))
+            { return; }
+            
+            powerupsController.PowerupActivated += OnPowerupActivated;
+            powerupsController.PowerupDeactivated += OnPowerupDeactivated;
+        }
+
+        private void OnDisable()
+        {
+            if (!ObjectUtilities.TryGetWithTag("Player", out GameObject playerObject))
+            { return; }
+            
+            if (!playerObject.TryGetComponent(out PowerupsController powerupsController))
+            { return; }
+                
+            powerupsController.PowerupActivated -= OnPowerupActivated;
+            powerupsController.PowerupDeactivated -= OnPowerupDeactivated;
+        }
+
+        private void Start()
+        {
+            if (TryGetComponent(out UIWorldObjectFollower objectFollowerComponent))
+            { objectFollowerComponent.Target = GameObject.FindWithTag("Player").transform; }
+        }
+
+        private bool TryGetFromPool(out ActivePowerupUI powerupUI)
+        {
+            for (int i = 0; i < pooledUI.Count; i++)
+            {
+                if (!pooledUI[i].gameObject.activeSelf)
+                { continue; }
+                
+                powerupUI = pooledUI[i];
+                return true;
+            }
+
+            powerupUI = null;
+            return false;
+        }
+
+        private void AddPowerup(ActivePowerup powerup)
+        {
+            ActivePowerupUI powerupUI = null;
+
+            if (!TryGetFromPool(out powerupUI))
+            {
+                powerupUI = Instantiate(activePowerupUIPrefab, activePowerupContainer);
+                pooledUI.Add(powerupUI);
+            }
+
+            powerupUI.Show(powerup);
+        }
+        
+        private void RemovePowerup(ActivePowerup powerup)
+        {
+            for (int i = 0; i < pooledUI.Count; i++)
+            {
+                if (!pooledUI[i].gameObject.activeSelf)
+                { continue; }
+
+                if (pooledUI[i].IsRepresentingPowerup(powerup))
+                { pooledUI[i].Hide(); }
+            }
+        }
+
+        private void OnPowerupActivated(ActivePowerup powerup) => AddPowerup(powerup);
+
+        private void OnPowerupDeactivated(ActivePowerup powerup) => RemovePowerup(powerup);
+    }
+}
