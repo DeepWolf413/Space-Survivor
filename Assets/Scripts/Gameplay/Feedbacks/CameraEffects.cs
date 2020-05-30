50224735a7c1d9a0768299f000827d9499bc1cb8
DeepWolf413 <jesper15fuji@live.dk>
2020-05-30 18:34:49 +0200

    Implemented camera shake when enemies die, and asteroids explode.

---
+++ Assets/Scripts/Gameplay/Feedbacks/CameraEffects.cs
@@ -0,0 +1,25 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace DeepWolf.SpaceSurvivor.Gameplay.Feedbacks
+{
+    public class CameraEffects : MonoBehaviour
+    {
+        private Vector3 originalPosition = Vector3.zero;
+
+        private new Transform transform;
+
+        private void Awake()
+        {
+            transform = base.transform;
+            originalPosition = transform.position;
+        }
+
+        public void Shake(float duration, Vector3 strength, int vibrato = 10, float randomness = 90.0f)
+        {
+            transform.DOComplete();
+            transform.position = originalPosition;
+            transform.DOShakePosition(duration, strength, vibrato, randomness);
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/Feedbacks/CameraShakeFeedback.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace DeepWolf.SpaceSurvivor.Gameplay.Feedbacks
+{
+    public class CameraShakeFeedback : Feedback
+    {
+        [SerializeField]
+        private float duration = 0.2f;
+
+        [SerializeField]
+        private Vector3 strength = new Vector3(0.3f, 0.3f, 0.0f);
+
+        [SerializeField]
+        private int vibrato = 10;
+
+        [SerializeField]
+        private float randomness = 90.0f;
+
+        private CameraEffects cameraEffectsComponent = null;
+
+        private void Start() => cameraEffectsComponent = Camera.main.GetComponent<CameraEffects>();
+
+        public override void Play() => cameraEffectsComponent.Shake(duration, strength, vibrato, randomness);
+    }
+}
